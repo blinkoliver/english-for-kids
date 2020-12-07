13db400e154ada64baaa2fa5be6537d70fffb426
blinkoliver <borovko28@gmail.com>
2020-12-08 00:00:53 +0300

    fix:fix after delete

---
+++ english-for-kids/README.md
@@ -0,0 +1,4 @@
+**App for learning english word**
+***1. Clone this repo***
+***2. npm or yarn install***
+***3. npm or yarn start***
---
+++ english-for-kids/src/app.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
+import renderCategoriesCards from './js/renderCategoriesCards.js';
+import burgerHandler from './js/burgerHandler.js';
+import gameMode from './js/gameMode.js';
+
+gameMode();
+burgerHandler();
+renderCategoriesCards();
