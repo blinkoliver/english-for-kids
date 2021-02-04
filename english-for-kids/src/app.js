000bf3ff0b49f9afca4010afe4e10816588e89d9
Anton Barouka <52159126+blinkoliver@users.noreply.github.com>
2021-02-04 15:54:29 +0300

    delete from main

--- english-for-kids/src/app.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable import/extensions */
-import renderCategoriesCards from './js/renderCategoriesCards.js';
-import burgerHandler from './js/burgerHandler.js';
-import gameMode from './js/gameMode.js';
-
-gameMode();
-burgerHandler();
-renderCategoriesCards();
